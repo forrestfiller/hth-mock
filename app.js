759c1b75c2965b888fa79be310b46ff2ac6ea1be
Forrest Filler <forrestfiller@users.noreply.github.com>
2016-10-22 15:06:42 -0400

    added in theme

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var labs = require('./routes/labs')
+var toolbox = require('./routes/toolbox')
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/labs', labs)
+app.use('/toolbox', toolbox)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
