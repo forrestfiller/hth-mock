70743e22c11b2f359f86baa853945f1054aed5dd
Forrest Filler <forrestfiller@users.noreply.github.com>
2016-10-23 14:00:21 -0400

    timestamp into lab scheme

--- models/Lab.js
+++ models/Lab.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose')
 var LabSchema = new mongoose.Schema({
   title: {type:String, defaul:''},
   text: {type:String, default:''},
-  type: {type:String, default:''}
+  type: {type:String, default:''},
+  timestamp: {type:Data, default:Date.now}
 })
 
 module.exports = mongoose.model('LabSchema', LabSchema)
